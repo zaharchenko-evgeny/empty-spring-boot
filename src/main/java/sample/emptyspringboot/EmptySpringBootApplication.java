f05198759e3e5553cbf07d573c70aa6dd2993b94
Evgenii Zakharchenko <evgenii.zakharchenko@jetbrains.com>
2025-07-05 13:29:32 +0200

    initial commit

---
+++ src/main/java/sample/emptyspringboot/EmptySpringBootApplication.java
@@ -0,0 +1,13 @@
+package sample.emptyspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmptySpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmptySpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sample/emptyspringboot/EmptySpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package sample.emptyspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmptySpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
