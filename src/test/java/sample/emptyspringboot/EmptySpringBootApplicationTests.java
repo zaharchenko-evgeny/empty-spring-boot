211c8f564fe6c69ff739b3465245f3c57381d663
Evgenii Zakharchenko <evgenii.zakharchenko@jetbrains.com>
2025-07-16 15:52:14 +0200

    Create broken test

--- src/test/java/sample/emptyspringboot/EmptySpringBootApplicationTests.java
+++ src/test/java/sample/emptyspringboot/EmptySpringBootApplicationTests.java
@@ -3,11 +3,14 @@ package sample.emptyspringboot;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class EmptySpringBootApplicationTests {
 
     @Test
     void contextLoads() {
+        assertTrue(false);
     }
 
 }
